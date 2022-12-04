5dde081950a25e776e1851b8911ca7ce68ea4e1e
L00171047 <113893999+L00171047@users.noreply.github.com>
2022-12-04 14:06:26 +0000

    Update App.java

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+    
+    public static int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static int subTwoNumbers(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplyTwoNumbers(int a, int b) {
+        return a*b;
+    }
+    
 }
